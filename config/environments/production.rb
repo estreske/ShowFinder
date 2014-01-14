dfcd22e6cc58cf6e1c72a8fc0b3f5e27f18ede80
Eric Streske <estreske@gmail.com>
2014-01-14 10:55:29 -0500

    fixed heroku precompile error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Showfinder::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.assets.precompile += %w( modernizr.js )
+  config.assets.precompile += %w( vendor/modernizr.js )
 end
